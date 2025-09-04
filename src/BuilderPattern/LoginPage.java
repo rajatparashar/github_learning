5e6ca7b2c9b14a6c3fa7df205481982b9edc7cce
Rajat Parashar <rajatparasharan@gmail.com>
2025-09-04 18:34:22 +0530

    updated changes as per reviewer comments

--- src/BuilderPattern/LoginPage.java
+++ src/BuilderPattern/LoginPage.java
@@ -1,6 +1,8 @@
 package BuilderPattern;
 
 public class LoginPage {
+	private String locator = "This is a dummy locator.";
+
 	public static void main(String[] args) {
 		int i = 10;
 		System.out.println("I'm in login page.java class file..." + i);
