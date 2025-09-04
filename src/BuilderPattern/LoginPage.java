8b1f618f502492b0cd1ea5165638a6460ac46209
Rajat Parashar <rajatparasharan@gmail.com>
2025-09-04 16:00:59 +0530

    Added a new file

---
+++ src/BuilderPattern/LoginPage.java
@@ -0,0 +1,8 @@
+package BuilderPattern;
+
+public class LoginPage {
+	public static void main(String[] args) {
+		int i = 10;
+		System.out.println("I'm in login page.java class file..." + i);
+	}
+}
