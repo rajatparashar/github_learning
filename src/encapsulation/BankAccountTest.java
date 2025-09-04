cc3928a6c4462ed6be136e5197235c601214c57e
Rajat Parashar <rajatparasharan@gmail.com>
2025-09-04 12:11:53 +0530

    updated value in BankAccountTest java file

--- src/encapsulation/BankAccountTest.java
+++ src/encapsulation/BankAccountTest.java
@@ -5,7 +5,7 @@ public class BankAccountTest {
 	public static void main(String[] args) {
 		BankAccount ba1 = new BankAccount();
 		ba1.setAccountNumber("12345678900");
-		ba1.setBalance(23.78);
+		ba1.setBalance(51.89);
 
 		System.out.println("Account Number is " + ba1.getAccountNumber());
 		System.out.println("Balance is " + ba1.getBalance());
