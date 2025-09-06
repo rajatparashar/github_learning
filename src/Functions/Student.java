a0317cfa654122ad2502bf66d9bfaa9ce46ef77b
Rajat Parashar <rajatparasharan@gmail.com>
2025-09-06 11:07:45 +0530

    Created Student.java file

---
+++ src/Functions/Student.java
@@ -0,0 +1,11 @@
+package Functions;
+
+public class Student {
+	static int age = 20;
+	static String name = "Rajat";
+
+	public static void main(String[] args) {
+		System.out.println("Student name is " + name);
+		System.out.println("Student age is " + age);
+	}
+}
